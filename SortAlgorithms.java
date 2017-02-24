f43e462e04dfcea8a6db3001c486a6b45c0e7714
abhishekmgva <abhishek.m@gvahealth.com>
2017-02-24 20:30:37 +0530

    bubble sort

--- SortAlgorithms.java
+++ SortAlgorithms.java
@@ -2,7 +2,7 @@ class SortAlgorithms{
 
   public static void main(String[] args) {
     int array[]={1,8,4,1,2,3,5,7,5,9,5,3,1};
-    selectionSort(array);
+    bubbleSort(array);
 
   }
 
@@ -36,6 +36,33 @@ class SortAlgorithms{
 
   }
 
+  /*
+  *Bubble Sort
+  *Dec:compare the correspoding element and swap if lower
+  *timeComplexity:O(n2)
+  *arr[]:array to be sorted
+  */
+    public static void bubbleSort(int arr[])
+    {
+      int lengthOfArray = arr.length;
+      printArray(arr);
+      for (int i=0;i<lengthOfArray-1;i++) {
+        for (int j=0;j<lengthOfArray-i-1;j++) {
+          if(arr[j] > arr[j+1])
+          {
+            int temp = arr[j];
+            arr[j] = arr[j+1];
+            arr[j+1] = temp;
+          }
+
+        }
+        printArray(arr);
+    }
+
+
+
+
+    }
 
 
   static void print(String toBePrinted)
